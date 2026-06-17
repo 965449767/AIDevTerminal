c29ed7fc8b21fbcbe03d0eadfd826eca90250503
AIDev Harness <aidev-harness@example.local>
2026-06-17 06:12:17 +0000

    fix: add dev-env/bin to Ubuntu PATH for camera-photo/pick access
    Problem: camera-photo script exists in dev-env/bin/ but Ubuntu proot
    container uses a hardcoded PATH that doesn't include this directory.
    
    Fix: Add /host-home/dev-env/bin to the PATH in both enter_ubuntu()
    and run_ubuntu_command() proot invocations. This makes all AIDev
    commands (camera-photo, camera-pick, ubuntu, etc.) available inside
    the Ubuntu container.

--- app/src/main/java/com/aidev/terminal/UbuntuBootstrapScripts.kt
+++ app/src/main/java/com/aidev/terminal/UbuntuBootstrapScripts.kt
@@ -341,7 +341,7 @@ AIDEV_PWD_HOOK_EOF
           exec "${'$'}AIDEV_PROOT" --link2symlink -0 -r "${'$'}AIDEV_ROOTFS" \
             -b /dev -b /proc -b /sys -b /sdcard -b /storage -b "${'$'}AIDEV_HOME:/host-home" \
             -w /root /usr/bin/env -i \
-            HOME=/root AIDEV_HOME=/host-home AIDEV_VERSION="${'$'}{AIDEV_VERSION:-unknown}" PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+            HOME=/root AIDEV_HOME=/host-home AIDEV_VERSION="${'$'}{AIDEV_VERSION:-unknown}" PATH=/host-home/dev-env/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
             TERM="${'$'}{TERM:-xterm-256color}" LANG=C.UTF-8 LC_ALL=C.UTF-8 "${'$'}shell" -l
         }
 
@@ -353,7 +353,7 @@ AIDEV_PWD_HOOK_EOF
           exec "${'$'}AIDEV_PROOT" --link2symlink -0 -r "${'$'}AIDEV_ROOTFS" \
             -b /dev -b /proc -b /sys -b /sdcard -b /storage -b "${'$'}AIDEV_HOME:/host-home" \
             -w /root /usr/bin/env -i \
-            HOME=/root AIDEV_HOME=/host-home AIDEV_VERSION="${'$'}{AIDEV_VERSION:-unknown}" PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+            HOME=/root AIDEV_HOME=/host-home AIDEV_VERSION="${'$'}{AIDEV_VERSION:-unknown}" PATH=/host-home/dev-env/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
             TERM="${'$'}{TERM:-xterm-256color}" LANG=C.UTF-8 LC_ALL=C.UTF-8 /bin/sh -lc "${'$'}*"
         }
 
